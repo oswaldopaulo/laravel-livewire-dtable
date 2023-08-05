07b5f0ab57a6025f2181f241b9d74caa291eef3c
oswaldopaulo <oswaldo.paulo@gmail.com>
2023-08-05 00:23:48 -0300

    init
    Signed-off-by: oswaldopaulo <oswaldo.paulo@gmail.com>

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </head>
+
+    <body class="antialiased">
+    <main>
+    <div class="container">
+    <div class="card">
+        <div class="card-title">Tabela</div>
+        <div class="card-header">tete</div>
+        <div class="card-body">
+            <div class="table-responsive">
+
+            </div>
+        </div>
+    </div>
+    </div>
+    </main>
+    </body>
+</html>
